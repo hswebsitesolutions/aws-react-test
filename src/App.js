a72a0e765da9c79dd5384dbe331d8afb369982ea
Harry <leandreamteam5@users.noreply.github.com>
2022-06-20 21:02:11 +0100

    Add some demo material ui components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@material-ui/core';
+import SaveIcon from '@material-ui/icons/Save'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button 
+          variant="contained" 
+          color="secondary"
+          startIcon={<SaveIcon />}>
+          Hello world
+        </Button>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
          This is a demo react app
